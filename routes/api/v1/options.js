aaef559e76d11e8a71d1719900864b7b67aa864e
soumyajitmandal101 <soumyajitmandal01@gmail.com>
2024-03-17 16:10:45 +0530

    project finish

---
+++ routes/api/v1/options.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose')
+const options=require('../../../models/options.js')
+
+if (options){
+    module.exports=options
+}
+else{
+    const optionSchema=new mongoose.Schema({
+
+        option:{
+            type:String,
+            required:true
+        },
+        question:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true
+        },
+        vote:{
+            type:Number,
+            default:0
+        },
+        add_vote:{
+            type:String,
+        }
+    })
+    
+    const Option=mongoose.model('Option',optionSchema);
+    module.exports=Option
+}
